ea740671e9352905e0c80f3871a64d060ebcd571
Agustín IABICHELLA ROBERT <iabichellarobert@gmail.com>
2022-07-11 20:21:06 -0300

    Primer Commit / Boiler + Back
    Primero acentamos las bases de la APP, tenemos por un lado el boiler del front, las rutas de POST/GET/DELETE en el back y el modelo correctamente seteado para recibir los POI en la DB (en esta ocasion usamos PSQL) (no olvidar crear el arhcivo .env).
    
    Con Insomia ya chequeeamos que podemos modificar entradas de POI para luego pedirlas desde el front, crearlas o borrarlas

---
+++ api/src/models/Poi.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "poi",
+    {
+      descripcion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+          len: [3,30],
+
+        }
+      },
+      direccion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      
+      },
+      telefono: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate:{
+           len: {
+               args: [10,13],
+               msg: "El numero de telefono tiene 10 digitos, hasta 13 contando el codigo de area"
+          }   
+        }
+      },
+      coordenadas: {
+        type: DataTypes.ARRAY(DataTypes.DECIMAL),
+        allowNull:false,
+        validate: {
+          is: /^((\-?|\+?)?\d+(\.\d+)?),\s*((\-?|\+?)?\d+(\.\d+)?)$/i,
+          
+        },
+        primaryKey: true,
+        unique:true
+      },
+      categoria: {
+        type: DataTypes.ENUM({
+          values: ['Comercial', 'Residencial','Mixto']
+        }),
+        allowNull:false
+      },
+    },
+    { timestamps: false }
+  );
+};
---
+++ api/src/routes/pois.js
@@ -0,0 +1,85 @@
+const { Router } = require("express");
+const { Poi } = require("../db");
+const router = Router();
+const { Op } = require("sequelize");
+// router.get("/", (req, res) => {
+//   res.send("Soy el get ;)");
+// });
+// Si esto no funciona intentar un try catch
+
+router.get("/", async (req, res, next) => {
+  const { descripcion } = req.body;
+  let result = null;
+  try {
+    if (descripcion) {
+      result = await Poi.findAll({
+        where: {
+          descripcion: {
+            [Op.iLike]: "%" + descripcion + "%",
+          },
+        },
+        order: [["descripcion", "ASC"]],
+      });
+      res.json(result);
+      if (result.length === 0) {
+        return res.send({ error: `El POI ${descripcion} no existe.` });
+      }
+    } else {
+      const response = await Poi.findAll({
+        order: [["descripcion", "ASC"]],
+      });
+
+      res.json(response);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/addPoi", async (req, res, next) => {
+  try {
+    const { descripcion, direccion, telefono, coordenadas, categoria } =
+      req.body;
+    if (descripcion && direccion && coordenadas && categoria) {
+      const [poi, created] = await Poi.create({
+        descripcion,
+        direccion,
+        coordenadas,
+        categoria,
+        telefono,
+        //form
+      });
+
+      return res.send("Actividad creada con exito");
+    } else {
+      res.json({ error: "Falta un elemento del form" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/delPoi", async (req, res, next) => {
+  const { descripcion, coordenadas } = req.body;
+  try {
+    if (descripcion && coordenadas) {
+      const elimPoi = await Poi.findOne({
+        where: {
+          descripcion,
+          coordenadas,
+        },
+      });
+
+      if (elimPoi) {
+        await elimPoi.destroy();
+        return res.json({ message: "POI eliminado correctamente" });
+      }
+    } else {
+      res.json({ error: "No se encontró este POI" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
